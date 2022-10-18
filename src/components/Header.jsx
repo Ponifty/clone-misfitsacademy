49b1321eb321ab0ebffe2713f524bcdc768c4055
P-O Mpolesha <po.mpolesha@gmail.com>
2022-10-18 19:52:03 +0200

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return <header className="header">
+    <ul>
+        <li>Misfits Academy</li>
+        <li>Cours</li>
+        <li>Inscriptions</li>
+        <li>Contact</li>
+    </ul>
+  </header>;
+}
